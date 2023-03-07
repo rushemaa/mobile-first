6bc520411e91a95b1a07ce0e930bc5efae23e043
rushemaa <rushemaa@gmail.com>
2023-03-07 12:08:17 +0200

    Fix the design

--- scripts.js
+++ scripts.js
@@ -3,11 +3,9 @@ const toolbar = document.querySelector('header');
 const clickedP = document.querySelector('.clicked-p');
 const navLinks = document.querySelectorAll('.clicked-ul li');
 function mobileMenu() {
-  toolbar.classList = 'hidden-toolbar';
   clicked.classList = 'clicked';
 }
 function hideMobileMenu() {
-  toolbar.classList = 'toolbar';
   clicked.classList = 'not-cliked';
 }
 
