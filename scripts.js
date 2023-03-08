ebdfc5cd26b43688b54d7f07e45c28be9ca443fe
rushemaa <rushemaa@gmail.com>
2023-03-08 12:23:36 +0200

    Add popup

--- scripts.js
+++ scripts.js
@@ -14,3 +14,83 @@ clickedP.addEventListener('click', hideMobileMenu);
 navLinks.forEach((element) => {
   element.addEventListener('click', hideMobileMenu);
 });
+// Opening and closing project popup
+const seeProject = document.querySelectorAll('.project-details');
+const main = document.querySelector('#main');
+function projectPopup() {
+  document.querySelector('#popup').classList.remove('hidden-popup');
+  document.querySelector('#popup').classList.add('popup');
+  main.classList = 'main';
+}
+function closePopup() {
+  document.querySelector('#main').classList.remove('main');
+  document.querySelector('#popup').classList.remove('popup');
+  document.querySelector('#popup').classList.add('hidden-popup');
+}
+seeProject.forEach((element) => {
+  element.addEventListener('click', projectPopup);
+});
+document.querySelector('#btn-close').addEventListener('click', closePopup);
+
+// Creating dynamic projec section
+
+let projects = [
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    mimage: 'images/projects/project-1.png',
+    image: 'images/projects/d-project-1.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: [
+      { text: 'See live', icon: 'images/socials/live.png' },
+      { text: 'See Source', icon: 'images/socials/github.png' },
+    ],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    mimage: 'images/projects/project-2.png',
+    image: 'images/projects/d-project-2.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: [
+      { text: 'See live', icon: 'images/socials/live.png' },
+      { text: 'See Source', icon: 'images/socials/github.png' },
+    ],
+  },
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    mimage: 'images/projects/project-3.png',
+    image: 'images/projects/d-project-3.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: [
+      { text: 'See live', icon: 'images/socials/live.png' },
+      { text: 'See Source', icon: 'images/socials/github.png' },
+    ],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    mimage: 'images/projects/project-4.png',
+    image: 'images/projects/d-project-4.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: [
+      { text: 'See live', icon: 'images/socials/live.png' },
+      { text: 'See Source', icon: 'images/socials/github.png' },
+    ],
+  },
+];
+// passing data into the works section
+const works = document.querySelector('.works');
+for (let index = 0; index < projects.length; index++) {
+  const pSection = document.createElement('section');
+  const pClasses = document.createAttribute('class');
+  pClasses.value = 'grid-items project-1 fonts';
+  pSection.setAttributeNode(pClasses);
+
+  works.appendChild(pSection);
+}
