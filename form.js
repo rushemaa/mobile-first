e90d8951d4868be534f0b71f325521bf1b69b9e9
rushemaa <rushemaa@gmail.com>
2023-03-09 10:35:10 +0200

    Update form.js

--- form.js
+++ form.js
@@ -1,2 +1,5 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {});
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
+});
